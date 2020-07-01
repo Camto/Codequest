a353702c6563f6e67c325e246374cd3abc49fca0
Camto <www.ben.pr@gmail.com>
2020-07-01 17:59:00 -0400

    Convert problems 1, 2, and tst to the new template.

--- Remade/2019/2.py
+++ Remade/2019/2.py
@@ -1,39 +1,30 @@
-import sys
-import math
-import string
-import re
-from functools import *
-from itertools import *
-from operator import *
-# Composition related.
-def comp(*funcs):
-	comp2 = lambda f, g: lambda *x: f(g(*x))
-	return reduce(comp2, funcs)
-tuplefy = lambda f: lambda args: f(*args)
-untuplefy = lambda f: lambda *args: f(args)
-def curry(n, f):
-	def wrap(*args):
-		if n > len(args):
-			return curry(n - len(args), lambda *largs: f(*args, *largs))
-		return f(*args)
-	return wrap
-curried = curry(2, curry)
-const = lambda val: lambda _: val
-# Utilities
+## Begin template https://github.com/Camto/Codequest .
+import sys; import math; import string; import re
+from functools import *; from itertools import *; from operator import *
+## Composition related.
+comp = lambda *funcs: reduce(lambda f, g: lambda *x: f(g(*x)), funcs)
+tuplefy = lambda f: lambda args: f(*args); untuplefy = lambda f: lambda *args: f(args)
+## Utility functions.
 map_maybe = lambda *args: filter(partial(ne, None), map(*args))
-concat_ = partial(reduce, concat)
 alpha = "abcdefghijklmnopqrstuvwxyz"
-fbool = comp(str.lower, str)
-rbool = partial(eq, "true")
-lmap = comp(list, map)
-lfilter = comp(list, filter)
-lrange = comp(list, range)
-lreversed = comp(list, reversed)
-lenumerate = comp(list, enumerate)
-lmap_maybe = comp(list, map_maybe)
-
-for _ in range(int(input())):
-	n1, n2 = map(int, input().split(" "))
+fbool = comp(str.lower, str); rbool = partial(eq, "true")
+lmap = comp(list, map); lfilter = comp(list, filter); lrange = comp(list, range)
+lreversed = comp(list, reversed); lenumerate = comp(list, enumerate); lmap_maybe = comp(list, map_maybe)
+## Input parsing.
+def parse_str(expect_type, s):
+	if expect_type in [int, float, str]:
+		return expect_type(s)
+	elif expect_type is bool:
+		return rbool(s)
+	elif type(expect_type) is tuple:
+		inp = s.split(" ")
+		return tuple(map(tuplefy(parse_str), zip(expect_type, inp)))
+	elif type(expect_type) is list:
+		return lmap(lambda _: get_in(expect_type[0]), range(int(s)))
+get_in = lambda expect_type: parse_str(expect_type, input())
+for given in get_in([(int, int)]):
+## End template.
+	n1, n2 = given
 	if n1 == n2:
 		print(2 * (n1 + n2))
 	else:
