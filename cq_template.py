76b11b1f0ed2a6de3785cfc20303ab01176874c2
Camto <www.ben.pr@gmail.com>
2020-07-01 15:50:53 -0400

    Remove `curry`, `curried`, `const`, and `concat_`.
    They took space and were near unused. Also some newline were taken out of input parsing.

--- cq_template.py
+++ cq_template.py
@@ -6,23 +6,14 @@ import re
 from functools import *
 from itertools import *
 from operator import *
-# Composition related.
+## Composition related.
 def comp(*funcs):
 	comp2 = lambda f, g: lambda *x: f(g(*x))
 	return reduce(comp2, funcs)
 tuplefy = lambda f: lambda args: f(*args)
 untuplefy = lambda f: lambda *args: f(args)
-def curry(n, f):
-	def wrap(*args):
-		if n > len(args):
-			return curry(n - len(args), lambda *largs: f(*args, *largs))
-		return f(*args)
-	return wrap
-curried = curry(2, curry)
-const = lambda val: lambda _: val
-# Utilities
+## Utility functions.
 map_maybe = lambda *args: filter(partial(ne, None), map(*args))
-concat_ = partial(reduce, concat)
 alpha = "abcdefghijklmnopqrstuvwxyz"
 fbool = comp(str.lower, str)
 rbool = partial(eq, "true")
@@ -32,10 +23,8 @@ lrange = comp(list, range)
 lreversed = comp(list, reversed)
 lenumerate = comp(list, enumerate)
 lmap_maybe = comp(list, map_maybe)
-# Input parsing.
-parse_list = lambda t, l: lmap(
-	lambda _: get_in(t),
-	range(l))
+## Input parsing.
+parse_list = lambda t, l: lmap(lambda _: get_in(t), range(l))
 def get_in(expect_type):
 	if expect_type in [int, float, str]:
 		return expect_type(input())
@@ -45,9 +34,7 @@ def get_in(expect_type):
 		inp = input().split(" ")
 		return tuple(map(
 			tuplefy(lambda t, i:
-				t(i)
-					if type(t) is not list
-					else parse_list(t[0], int(i))),
+				t(i) if type(t) is not list else parse_list(t[0], int(i))),
 			zip(expect_type, inp)))
 	elif type(expect_type) is list:
 		return parse_list(expect_type[0], get_in(int))
--- cq_template_fancy.py
+++ cq_template_fancy.py
@@ -1,3 +1,5 @@
+## Imports
+
 # Basic imports.
 import sys
 import math
@@ -9,6 +11,8 @@ from functools import *
 from itertools import *
 from operator import *
 
+## Composition related.
+
 # Function composition.
 def comp(*funcs):
 	comp2 = lambda f, g: lambda *x: f(g(*x))
@@ -18,25 +22,10 @@ def comp(*funcs):
 tuplefy = lambda f: lambda args: f(*args)
 untuplefy = lambda f: lambda *args: f(args)
 
-# Function currying.
-def curry(n, f):
-	def wrap(*args):
-		if n > len(args):
-			return curry(n - len(args), lambda *largs: f(*args, *largs))
-		return f(*args)
-	return wrap
-	
-# Curry as a decorator.
-curried = curry(2, curry)
-
-# The constant function.
-const = lambda val: lambda _: val
+## Utility functions.
 
 # Map and filter combined.
 map_maybe = lambda *args: filter(partial(ne, None), map(*args))
-
-# Haskell-like concat.
-concat_ = partial(reduce, concat)
 
 # The alphabet.
 alpha = "abcdefghijklmnopqrstuvwxyz"
@@ -53,12 +42,11 @@ lreversed = comp(list, reversed)
 lenumerate = comp(list, enumerate)
 lmap_maybe = comp(list, map_maybe)
 
+## Input parsing.
+
+parse_list = lambda t, l: lmap(lambda _: get_in(t), range(l))
+
 # Parse all the problem's input at once.
-
-parse_list = lambda t, l: lmap(
-	lambda _: get_in(t),
-	range(l))
-
 def get_in(expect_type):
 	if expect_type in [int, float, str]:
 		return expect_type(input())
@@ -68,9 +56,7 @@ def get_in(expect_type):
 		inp = input().split(" ")
 		return tuple(map(
 			tuplefy(lambda t, i:
-				t(i)
-					if type(t) is not list
-					else parse_list(t[0], int(i))),
+				t(i) if type(t) is not list else parse_list(t[0], int(i))),
 			zip(expect_type, inp)))
 	elif type(expect_type) is list:
 		return parse_list(expect_type[0], get_in(int))
