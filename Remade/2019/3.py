ec506992cdf7d5bbe73b97ef55d88e2ae69ab970
Camto <www.ben.pr@gmail.com>
2020-07-01 16:24:56 -0400

    Starting conversion to new template (`bool` is broken)

--- Remade/2019/3.py
+++ Remade/2019/3.py
@@ -1,37 +1,32 @@
-import sys
-import math
-import string
-import re
-from functools import *
-from itertools import *
-from operator import *
-# Composition related.
-def comp(*funcs):
-	comp2 = lambda f, g: lambda *x: f(g(*x))
-	return reduce(comp2, funcs)
-tuplefy = lambda f: lambda args: f(*args)
-untuplefy = lambda f: lambda *args: f(args)
-def curry(n, f):
-	def wrap(*args):
-		if n > len(args):
-			return curry(n - len(args), lambda *largs: f(*args, *largs))
-		return f(*args)
-	return wrap
-curried = curry(2, curry)
-const = lambda val: lambda _: val
-# Utilities
+## Begin template https://github.com/Camto/Codequest .
+import sys; import math; import string; import re
+from functools import *; from itertools import *; from operator import *
+## Composition related.
+comp = lambda *funcs: reduce(lambda f, g: lambda *x: f(g(*x)), funcs)
+tuplefy = lambda f: lambda args: f(*args); untuplefy = lambda f: lambda *args: f(args)
+## Utility functions.
 map_maybe = lambda *args: filter(partial(ne, None), map(*args))
-concat_ = partial(reduce, concat)
 alpha = "abcdefghijklmnopqrstuvwxyz"
-fbool = comp(str.lower, str)
-rbool = partial(eq, "true")
-lmap = comp(list, map)
-lfilter = comp(list, filter)
-lrange = comp(list, range)
-lreversed = comp(list, reversed)
-lenumerate = comp(list, enumerate)
-lmap_maybe = comp(list, map_maybe)
-
-for _ in range(int(input())):
-	bool1, bool2 = map(rbool, input().split(" "))
+fbool = comp(str.lower, str); rbool = partial(eq, "true")
+lmap = comp(list, map); lfilter = comp(list, filter); lrange = comp(list, range)
+lreversed = comp(list, reversed); lenumerate = comp(list, enumerate); lmap_maybe = comp(list, map_maybe)
+## Input parsing.
+parse_list = lambda t, l: lmap(lambda _: get_in(t), range(l))
+def get_in(expect_type):
+	if expect_type in [int, float, str]:
+		return expect_type(input())
+	elif expect_type is bool:
+		return rbool(input())
+	elif type(expect_type) is tuple:
+		inp = input().split(" ")
+		print(list(zip(expect_type, inp)))
+		return tuple(map(
+			tuplefy(lambda t, i: t(i) if type(t) is not list else parse_list(t[0], int(i))),
+			zip(expect_type, inp)))
+	elif type(expect_type) is list:
+		return parse_list(expect_type[0], get_in(int))
+for given in get_in([(bool, bool)]):
+## End template.
+	bool1, bool2 = given
+	print(given)
 	print(fbool(bool1 == bool2))
