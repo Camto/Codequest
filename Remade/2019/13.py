812834aec5f695f8805e36d27b743c23fa34b7ab
Camto <www.ben.pr@gmail.com>
2020-07-01 18:26:10 -0400

    Convert the other remade problems to the new template.
    `get_in` is fantastic, and will be getting even better! The next feature will be used as `get_in([int, str])` for problem 10, having the items in the pair be on separate lines.

--- Remade/2019/13.py
+++ Remade/2019/13.py
@@ -1,40 +1,32 @@
-import sys
-import math
-import string
-import re
-from functools import *
-from itertools import *
-from operator import *
-# Composition related.
-def comp(*funcs):
-	comp2 = lambda f, g: lambda *x: f(g(*x))
-	return reduce(comp2, funcs)
-tuplefy = lambda f: lambda args: f(*args)
-untuplefy = lambda f: lambda *args: f(args)
-def curry(n, f):
-	def wrap(*args):
-		if n > len(args):
-			return curry(n - len(args), lambda *largs: f(*args, *largs))
-		return f(*args)
-	return wrap
-curried = curry(2, curry)
-const = lambda val: lambda _: val
-# Utilities
+## Begin template https://github.com/Camto/Codequest .
+import sys; import math; import string; import re
+from functools import *; from itertools import *; from operator import *
+## Composition related.
+comp = lambda *funcs: reduce(lambda f, g: lambda *x: f(g(*x)), funcs)
+tuplefy = lambda f: lambda args: f(*args); untuplefy = lambda f: lambda *args: f(args)
+## Utility functions.
 map_maybe = lambda *args: filter(partial(ne, None), map(*args))
-concat_ = partial(reduce, concat)
 alpha = "abcdefghijklmnopqrstuvwxyz"
-fbool = comp(str.lower, str)
-rbool = partial(eq, "true")
-lmap = comp(list, map)
-lfilter = comp(list, filter)
-lrange = comp(list, range)
-lreversed = comp(list, reversed)
-lenumerate = comp(list, enumerate)
-lmap_maybe = comp(list, map_maybe)
+fbool = comp(str.lower, str); rbool = partial(eq, "true")
+lmap = comp(list, map); lfilter = comp(list, filter); lrange = comp(list, range)
+lreversed = comp(list, reversed); lenumerate = comp(list, enumerate); lmap_maybe = comp(list, map_maybe)
+## Input parsing.
+def parse_str(expect_type, s):
+	if expect_type in [int, float, str]:
+		return expect_type(s)
+	elif expect_type is bool:
+		return rbool(s)
+	elif type(expect_type) is tuple:
+		inp = s.split(" ")
+		return tuple(map(tuplefy(parse_str), zip(expect_type, inp)))
+	elif type(expect_type) is list:
+		return lmap(lambda _: get_in(expect_type[0]), range(int(s)))
+get_in = lambda expect_type: parse_str(expect_type, input())
+## End template.
 
-for _ in range(int(input())):
-	rows, cols, bombs = map(int, input().split(" "))
-	locs = lmap(lambda _: tuple(map(comp(partial(add, 1), int), input().split(" "))), range(bombs))
+for given in get_in([(int, int, [(int, int)])]):
+	rows, cols, locs = given
+	locs = lmap(tuplefy(lambda x, y: (x + 1, y + 1)), locs)
 	board = lmap(lambda _: [0] * (cols + 2), range(rows + 2))
 	
 	for (row, col) in locs:
@@ -44,4 +36,4 @@ for _ in range(int(input())):
 	
 	buf = lmap(tuplefy(lambda r, row: lmap(tuplefy(lambda c, n: str(n) if (r, c) not in locs else "*"), lenumerate(row)[1:-1])), lenumerate(board)[1:-1])
 	
-	print("\n".join(map(concat_, buf)))
+	print("\n".join(map(partial(reduce, concat), buf)))
